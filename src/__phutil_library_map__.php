9f766b0c13e3b142c321611c3ecc4b6256bfd029
Mailson Menezes <mailson.menezes@openbossa.org>
2013-01-02 19:43:37 -0300

    [irc] Channel requests are displayed on channel
    Messages delivered on channel metioning a user are now displayed on the
    channel
    
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- src/__phutil_library_map__.php
+++ src/__phutil_library_map__.php
@@ -12,6 +12,7 @@ phutil_register_library_map(array(
   array(
     'ConduitAPI_differential_getcommitdata_Method' => 'conduit/ConduitAPI_differential_getcommitdata_Method.php',
     'ExtendedIRCDifferentialNotificationHandler' => 'ircbot/ExtendedIRCDifferentialNotificationHandler.php',
+    'INDTIRCObjectNameHandler' => 'ircbot/INDTPhabricatorIRCObjectNameHandler.php',
     'PhabricatorApplicationRegister' => 'register/application/PhabricatorApplicationRegister.php',
     'PhabricatorApplicationSendmail' => 'sendmail/application/PhabricatorApplicationSendmail.php',
     'RegisterController' => 'register/controller/RegisterController.php',
@@ -24,6 +25,7 @@ phutil_register_library_map(array(
   array(
     'ConduitAPI_differential_getcommitdata_Method' => 'ConduitAPIMethod',
     'ExtendedIRCDifferentialNotificationHandler' => 'PhabricatorIRCHandler',
+    'INDTIRCObjectNameHandler' => 'PhabricatorIRCHandler',
     'PhabricatorApplicationRegister' => 'PhabricatorApplication',
     'PhabricatorApplicationSendmail' => 'PhabricatorApplication',
     'RegisterController' => 'PhabricatorController',
